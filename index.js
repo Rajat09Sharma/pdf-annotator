df6fef14652be95efe0cd93df7908d3160c4e2bb
Rajat Sharma <rajat9.lyfe@gmail.com>
2025-09-17 02:25:46 +0530

    port added in .env

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const authRouter = require("./routes/auth");
 const UserRouter = require("./routes/user");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT||3000;
 
 connectDB();
 
